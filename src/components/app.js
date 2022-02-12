5d59b6c5779957512f3b99514e7ded90b90372a9
Dain Duncan <30823030+dainduncan-dev@users.noreply.github.com>
2022-02-11 22:16:43 -0600

    main

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+import NavigationContainer from "./navigation/navigation-container.js";
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this)
+  }
+  getPortfolioItems() {
+    axios
+      .get("https://garyduncan.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        // handle success
+        console.log("response data", response);
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      });
+  }
   render() {
+    this.getPortfolioItems();
     return (
       <div className="app">
-        <h1>Gary Duncan Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <Router>
+          <div>
+            <h1>Gary Duncan Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
