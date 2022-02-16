2c5030acde3c4c5f35d621a3a930852005c1d05d
Dain Duncan <30823030+dainduncan-dev@users.noreply.github.com>
2022-02-15 21:51:54 -0600

    Added variables and link styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,20 +10,29 @@ export default class NavigationContainer extends Component {
         return (
           <div className="nav-wrapper">
             <div className="left-side">
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-              <NavLink to="/about-me" activeClassName="nav-link-active">
-                About
-              </NavLink>
-              <NavLink to="/contact" activeClassName="nav-link-active">
-                Contact
-              </NavLink>
-              <NavLink to="/blog" activeClassName="nav-link-active">
-                Blog
-              </NavLink>
+              <div className="nav-link-wrapper">
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              </div>
 
-              {false ? <button>Add Blog</button> : null}
+              <div className="nav-link-wrapper">
+                <NavLink to="/about-me" activeClassName="nav-link-active">
+                  About
+                </NavLink>
+              </div>
+
+              <div className="nav-link-wrapper">
+                <NavLink to="/contact" activeClassName="nav-link-active">
+                  Contact
+                </NavLink>
+              </div>
+              
+              <div className="nav-link-wrapper">
+                <NavLink to="/blog" activeClassName="nav-link-active">
+                  Blog
+                </NavLink>
+              </div>
             </div>
 
             <div className="right-side">Gary Duncan</div>
