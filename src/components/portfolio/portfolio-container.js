b498a0879e14ca71b59c9d2db871f2a53cee1223
Dain Duncan <30823030+dainduncan-dev@users.noreply.github.com>
2022-02-15 23:05:25 -0600

    Added logos and description styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -75,11 +75,7 @@ export default class PortfolioContainer extends Component {
           Enterprise
         </button>
 
-        <div className="portfolio-items-wrapper">
-          
-        </div>
-
-        {this.portfolioItems()}
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
       </div>
     );
   }
