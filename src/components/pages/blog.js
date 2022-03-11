f936bf2de6b83a5d77f84b2216a0cfd05f6697df
Gary Duncan <dainduncan@gmail.com>
2022-03-10 21:28:28 -0600

    Implemented Blog Modal

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
@@ -11,22 +13,42 @@ class Blog extends Component {
       blogItems: [],
       totalCount: 0,
       currentPage: 0,
+      isLoading: true,
+      blogModalIsOpen: false
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
   }
 
-  activateInfiniteScroll() {
-    window.onscroll = () => {
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    })
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
+  }
+
+  onScroll() {
+      if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+        return;
+      }
+
+
       if (
         window.innerHeight + document.documentElement.scrollTop ===
         document.documentElement.offsetHeight
       ) {
-        console.log("get more posts");
+        this.getBlogItems();
       }
     };
-  }
 
   getBlogItems() {
     this.setState({
@@ -34,13 +56,15 @@ class Blog extends Component {
     });
 
     axios
-      .get("https://garyduncan.devcamp.space/portfolio/portfolio_blogs", {
+      .get(`https://garyduncan.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
         withCredentials: true,
       })
       .then((response) => {
+        console.log("getting", response.data);
         this.setState({
-          blogItems: response.data.portfolio_blogs,
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
+          isLoading: false
         });
       })
       .catch((error) => {
@@ -52,6 +76,10 @@ class Blog extends Component {
     this.getBlogItems();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false)
+  }
+
   render() {
     const blogRecords = this.state.blogItems.map((blogItem) => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
@@ -59,7 +87,20 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
+        <BlogModal 
+        handleModalClose={this.handleModalClose}
+        modalIsOpen={this.state.blogModalIsOpen} />
+
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>Open Modal</a>  
+        </div>
+
         <div className="content-container">{blogRecords}</div>
+
+        {this.state.isLoading ? (
+        <div className="content-loader">
+          <FontAwesomeIcon icon="spinner" spin />
+        </div>) : null }
       </div>
     );
   }
