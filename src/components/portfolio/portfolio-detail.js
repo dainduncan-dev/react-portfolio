64e030c0362526545e41407ad9cd023b0e61fdb2
Gary Duncan <dainduncan@gmail.com>
2022-03-16 21:37:07 -0500

    Added data functionality into portfolio detail page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,48 @@
 import React from "react";
+import axios from "axios";
 
-export default function (props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portfolioItem: {}
+    }
+  }
+
+  componentDidMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(`https://garyduncan.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,{ withCredentials: true })
+      .then((response) => {
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        })
+      })
+      .catch((err) => {
+        console.log("getportfolioItem error", err);
+      });
+  }
+
+  render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url,
+    } = this.state.portfolioItem;
+
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </div>
+    );
+  }
 }
